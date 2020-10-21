3b279c207de47be0ece17d58f9c52cea5b55c4a7
alfredopl <aalfredoplz@gmail.com>
2020-10-20 22:21:30 -0300

    other form in suma_pares.rb file

--- suma_pares.rb
+++ suma_pares.rb
@@ -1,9 +1,11 @@
 n = ARGV[0].to_i
 acum = 0
+ac=0
 i=0
-while i <= n 
-    acum+=n
+while i < n 
+    acum+= 2
+    ac +=acum
     puts acum
     i+=1
 end        
-puts"la suma total es #{acum}"
+puts"la suma total es #{ac}"
