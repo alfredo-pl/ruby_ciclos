19616f0276074a06982beb04c5a82429945c96fd
alfredopl <aalfredoplz@gmail.com>
2020-10-21 09:43:37 -0300

    other form gen.rb file

--- gen.rb
+++ gen.rb
@@ -1,11 +1,5 @@
 def gen(n)
-array=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','z']
-acum=''
-i=0
-while i < n
-    acum+=array[i]
-    i+=1
-end
-acum
+abc='abcdefghijklmnopqrstuvwxyz'
+abc[0,n]
 end
 puts gen(ARGV[0].to_i)
